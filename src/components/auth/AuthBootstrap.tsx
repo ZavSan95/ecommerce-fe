3d018f37e6c4f024ee4e075a3e251c161c7f4230
Santiago Zavatti <santy92_59@hotmail.com>
2026-02-10 08:23:15 -0300

    fix authbootstrap

--- src/components/auth/AuthBootstrap.tsx
+++ src/components/auth/AuthBootstrap.tsx
@@ -5,33 +5,22 @@ import { useAppDispatch } from '@/store/hooks';
 import {
   setUser,
   clearAuth,
-  setInitialized,
 } from '@/store/auth/authSlice';
 import { authMe } from '@/services/auth.service';
-import { hasAuthCookie } from '@/utils/authCookies';
 
 export function AuthBootstrap({ children }: { children: React.ReactNode }) {
-  console.log('[AUTH BOOTSTRAP] mounted');
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (!hasAuthCookie()) {
-      dispatch(clearAuth());
-      dispatch(setInitialized());
-      return;
-    }
-
     authMe()
       .then(({ user }) => {
         dispatch(setUser(user));
       })
       .catch(() => {
         dispatch(clearAuth());
-      })
-      .finally(() => {
-        dispatch(setInitialized());
       });
   }, [dispatch]);
 
   return <>{children}</>;
 }
+
